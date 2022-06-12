9dd04ebc702361ffd9dca75ecc5f8b2a78c118af
JN513 <jn5.guape@gmail.com>
2022-06-11 22:34:13 -0300

    thread para download do spotify

--- core/thread.py
+++ core/thread.py
@@ -1,5 +1,5 @@
 from PyQt5.QtCore import pyqtSignal, QObject
-from core.core import download_instagram, download_youtube
+from core.core import download_instagram, download_youtube, download_spotify
 
 
 class YoutubeDownloadThread(QObject):
@@ -50,6 +50,31 @@ class InstagramDownloadThread(QObject):
         erro = ""
         if error > 0:
             erro += f" Erro ao baixar {error} imagem(s)/video(s)."
+
+        self.alert.emit(
+            "Download(s) Concluidos.",
+            "Todos os downloads possiveis foram finalizados." + erro,
+        )
+
+        self.finished.emit()
+
+
+class SpotifyDownloadThread(QObject):
+    finished = pyqtSignal()
+    alert = pyqtSignal(str, str)
+
+    def __init__(self, links, path_to_save, format_to_Save = "mp3"):
+        super().__init__()
+        self.path_to_save = path_to_save
+        self.links = links
+        self.format_to_Save = format_to_Save
+
+    def run(self):
+        ok, error = download_spotify(self.links, self.path_to_save, self.format_to_Save)
+
+        erro = ""
+        if error > 0:
+            erro += f" Erro ao baixar musica(s)."
 
         self.alert.emit(
             "Download(s) Concluidos.",
